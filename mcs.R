364500270f24974f592a7f6c3a69f8157395556b
Tainan Boff <tainanboff@MacBook-de-Tainan.local>
2018-07-23 00:55:45 -0300

    correcao do mcs

--- mcs.R
+++ mcs.R
@@ -19,40 +19,41 @@ library(parallel)
 # Baixa os dados do GitHub
 source_data("https://github.com/TainanBoff/TCC/blob/master/perdas.RData?raw=True")
 
-# Identifica o numero de cores (menos 1) para usar processamento em paralelo
-cores <- detectCores() - 1
+# Identifica o numero de cl (menos 1) para usar processamento em paralelo e cria cluster
+cores <- detectcl() - 1
+cl <- makeCluster(cores)
 
 # Roda o Model Confidence Set e salva os resultados para cada acao. 
 # Tem que inserir o link da pasta onde os resultados serao salvos.
 
-MCS.itub4 <- MCSprocedure(perda.itub4, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cores)
+MCS.itub4 <- MCSprocedure(perda.itub4, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cl)
 saveRDS(MCS.itub4, "/mcs_itub4.RDS")
 
-MCS.trpl4 <- MCSprocedure(perda.trpl4, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cores)
+MCS.trpl4 <- MCSprocedure(perda.trpl4, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cl)
 saveRDS(MCS.trpl4, "/mcs_trpl4.RDS")
 
-MCS.vale3 <- MCSprocedure(perda.vale3, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cores)
+MCS.vale3 <- MCSprocedure(perda.vale3, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cl)
 saveRDS(MCS.vale3, "/mcs_vale3.RDS")
 
-MCS.csmg3 <- MCSprocedure(perda.csmg3, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cores)
+MCS.csmg3 <- MCSprocedure(perda.csmg3, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cl)
 saveRDS(MCS.csmg3, "/mcs_csmg3.RDS")
 
-MCS.bbdc4 <- MCSprocedure(perda.bbdc4, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cores)
+MCS.bbdc4 <- MCSprocedure(perda.bbdc4, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cl)
 saveRDS(MCS.bbdc4, "/mcs_bbdc4.RDS")
 
-MCS.btow3 <- MCSprocedure(perda.btow3, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cores)
+MCS.btow3 <- MCSprocedure(perda.btow3, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cl)
 saveRDS(MCS.btow3, "/mcs_btow3.RDS")
 
-MCS.lren3 <- MCSprocedure(perda.lren3, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cores)
+MCS.lren3 <- MCSprocedure(perda.lren3, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cl)
 saveRDS(MCS.lren3, "/mcs_lren3.RDS")
 
-MCS.pssa3 <- MCSprocedure(perda.pssa3, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cores)
+MCS.pssa3 <- MCSprocedure(perda.pssa3, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cl)
 saveRDS(MCS.pssa3, "/mcs_pssa3.RDS")
   
-MCS.vale5 <- MCSprocedure(perda.vale5, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cores)
+MCS.vale5 <- MCSprocedure(perda.vale5, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cl)
 saveRDS(MCS.vale5, "/mcs_vale5.RDS")
 
-MCS.bbdc3 <- MCSprocedure(perda.bbdc3, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cores)
+MCS.bbdc3 <- MCSprocedure(perda.bbdc3, alpha = 0.05, B = 10000, statistic = "TR", k = NULL, cl = cl)
 saveRDS(MCS.trpl4, "/mcs_bbdc3.RDS")
 
 stopCluster(cl)
